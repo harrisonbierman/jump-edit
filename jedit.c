963473a9b713a7f7a27ffe7913d5851d710f6040
harrison <harryscout@gmail.com>
2025-04-18 20:11:23 -0700

    before removing inner loop

--- jedit.c
+++ jedit.c
@@ -10,20 +10,14 @@
 #include <string.h>
 
 int main(int argsc, char **argsv) {
+
+	// Extract Argument
 	for(int i = 0; i < argsc; i++) {
-
-		printf("Pointer: %p\n", argsv[i]);
-		printf("sizeof(Pointer) %zu\n", sizeof(argsv[i]));
-		printf("distance to next pointer: %ld\n", argsv[i+1] - argsv[i]);
-
-		char buf[64] = {0};
+		char buf[1024] = {0};
 		size_t arg_len = strnlen(argsv[i], sizeof(buf) - 1);
-		printf("arg_len: %zu \n", arg_len);
 		for (size_t j = 0; j < arg_len; j++) {
-			buf[j] = argsv[i][j];	
+			buf[j] = argsv[i][j]; 	
 		}
-
-		printf("Argument %d: %s\n\n", i, buf); 
 
 	}
 
