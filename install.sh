bb4bc20ec3d449e7a57ea814000011c9df67583a
harrison-pc <harryscout@gmail.com>
2025-05-11 17:13:42 -0700

    fix(bash): fix bug where '-h' flag was not recognized by bash script

--- install.sh
+++ install.sh
@@ -9,15 +9,21 @@ tee /etc/profile.d/jump_edit.sh >/dev/null << 'EOF'
 #!/usr/bin/env bash
 je() {
 
+	# checking if normal je command 
 	if [[ $1 == add ||
 		$1 == list ||
-		$1 == --help ||
 		$1 == rm ||
-		$1 == default-editor ]]; then
+		$1 == default-editor ||
+		$1 == --help ||
+		$1 == -h
+		]]; 
+	then
 		jump_edit "$@"
 		return
 	fi
 
+	# if jump label is being used
+	# read stdout and run it
 	local script
 	script="$(jump_edit "$@")" || return
 
@@ -29,3 +35,4 @@ chmod +x /etc/profile.d/jump_edit.sh
 echo "jump_edit binary /usr/local/bin/jump_edit installed"
 echo "jump_edit bash function /etc/profile.d/jump_edit.sh created."
 echo "Start a new shell or run: source /etc/profile.d/jump_edit.sh"
+
